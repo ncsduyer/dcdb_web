a4ef2a72b2e70ef6a8b58ccae6e1d8029f999a7b
libo <418431198@qq.com>
2019-05-25 17:57:49 +0800

    dcdb-web 1.0.1 2019-5-25

--- js/url.js
+++ js/url.js
@@ -123,6 +123,7 @@ var apiUrl = {
 
 	//POST 交办事项列表
 	ApiTaskAdd: apiPrefix + "/task/add",
+	ApiTaskEdit: apiPrefix + "/task/update",
 
 	//POST 事项名称列表
 	ApiTaskNameList: apiPrefix + "/task/getTaskList",
@@ -130,6 +131,7 @@ var apiUrl = {
 
 	//POST 督办事项责任单位列表
 	ApiTaskAssignUnitList: apiPrefix + "/taskassignUnit/list",
+	ApiTaskAssignUnitDetail: apiPrefix + "/taskassignUnit/detail/",
 
     //GET 获取交办事项详情
     ApiTaskDetailList: apiPrefix + "/task/detail",
@@ -201,6 +203,7 @@ var apiUrl = {
 	ApidocsXlsExport: apiPrefix + "/docs/export",
 	//信息報表導出
 	ApiinfosXlsExport: apiPrefix + "/infos/export",
+	ApiinfosXlsDetail: apiPrefix + "/infos/detail",
 	  // 区委信息删除
     Apiinfosdelete: apiPrefix + "/infosrec/delete",
 	// 区委信息詳情
@@ -543,10 +546,25 @@ function getDateStringByDateTime(data,start,end){
 function html_cancel() {
 	var index = parent.layer.getFrameIndex(window.name);
 	// parent.LoadTable();
-	if (typeof parent.LoadTable != 'undefined' && parent.LoadTable instanceof Function) {
-		parent.LoadTable();
-	}
-
 	parent.layer.close(index);
+	parent.LoadTable();
 	parent.location.reload();
+}
+function dateFmamt(fmt,date)
+{ //author: meizz
+	var o = {
+		"M+" : date.getMonth()+1,                 //月份
+		"d+" : date.getDate(),                    //日
+		"h+" : date.getHours(),                   //小时
+		"m+" : date.getMinutes(),                 //分
+		"s+" : date.getSeconds(),                 //秒
+		"q+" : Math.floor((date.getMonth()+3)/3), //季度
+		"S"  : date.getMilliseconds()             //毫秒
+	};
+	if(/(y+)/.test(fmt))
+		fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));
+	for(var k in o)
+		if(new RegExp("("+ k +")").test(fmt))
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+	return fmt;
 }
